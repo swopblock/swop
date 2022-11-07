3c665838c909e85cba24aea13ebc62e7cc1cc139
unknown <bjames.12299@gmail.com>
2022-11-07 14:25:34 -0800

    Network Code

---
+++ swop/Net/Network.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Diagnostics;
+
+namespace Swopblock.Net
+{
+    public class Network
+    {
+        public List<string> Peers { get; set; }
+
+        public ConcurrentQueue<Packet> RecievedPackets { get; set; }
+
+        private static readonly ushort port = 25233; //hotel room at money2020
+
+        bool watching = true;
+
+        int resendAttempts = 10;
+
+        UdpClient client = new UdpClient();
+
+        public Network(List<string> peers)
+        {
+            Peers = peers;
+            RecievedPackets = new ConcurrentQueue<Packet>();    
+        }
+
+        public void Setup()
+        {
+            if (Peers != null)
+            {
+                if (Peers.Count > 0)
+                {
+                    foreach (string ip in Peers)
+                    {
+                        try
+                        {
+                            IPAddress address = IPAddress.Parse(ip);
+
+                            client.Connect(new IPEndPoint(address, port));
+                        }
+                        catch (Exception ex)
+                        {
+                            Debug.WriteLine(ex.Message);
+                        }
+                    }
+                }
+            }
+        }
+
+        public void SendData(byte[] data)
+        {
+            int count = 0;
+
+            int attempt = 0;
+
+            while(count < data.Length)
+            {
+                byte[] rawPack = Packet.PackBytes(data);
+
+                count = client.Send(rawPack, rawPack.Length);
+
+                if(attempt++ > resendAttempts)
+                {
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(333 * attempt);
+                }
+            }
+        }
+
+        public void StartRecieving()
+        {
+            new Thread(() =>
+            {
+                IPEndPoint point = new IPEndPoint(IPAddress.Any, port);
+
+                UdpClient server = new UdpClient(point);
+
+                Thread.CurrentThread.IsBackground = true;
+
+                while (watching)
+                {
+                    byte[] data = server.Receive(ref point);
+
+                    if (data != null)
+                    {
+                        if (data.Length > 0)
+                        {
+                            Packet pkt = Packet.GetPacket(data);
+                            if (pkt != null)
+                            {
+                                RecievedPackets.Enqueue(pkt);
+
+                                Console.WriteLine(pkt.Readable);
+                            }
+                            else
+                            {
+                                //Console.WriteLine("[Failed]");
+                            }
+                        }
+                    }
+                }
+            }).Start();
+        }
+    }
+}
---
+++ swop/Net/Packet.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Swopblock.Intentions.Utilities;
+
+namespace Swopblock.Net
+{
+    public class Packet
+    {
+        public static string head = "[SwopBlock]";
+
+        public string Heading { get; set; }
+        public byte[] rawData { get; set; }
+        public byte[] data { get; set; }
+
+        public string Readable 
+        {   get
+            {
+                return Heading 
+                    + " " 
+                    + Length 
+                    + " " 
+                    + Utility.ConvertToReadable(data) 
+                    + " "
+                    + Checksum;
+            }
+            private set { }
+        }
+
+        public int Length = 0;
+        public int Checksum = 0;
+
+        public static byte[] PackBytes(byte[] data)
+        {
+            List<byte> bytes = new List<byte>();
+
+            for (int i = 0; i < head.Length; i++)
+            {
+                bytes.Add((byte)head[i]);
+            }
+
+            bytes.AddRange(BitConverter.GetBytes(data.Length));
+            bytes.AddRange(data);
+            bytes.AddRange(BitConverter.GetBytes(checksum(data)));
+
+            return bytes.ToArray();
+        }
+
+        public static Packet GetPacket(byte[] data)
+        {
+            Packet packet = null;
+
+            if(data.Length > 8)
+            {
+                string header = Utility.ConvertToString(Utility.GetNextByteSet(data, 0, head.Length));
+                
+                if (header == head)
+                {
+                    int len = BitConverter.ToInt32(data, head.Length);
+                    byte[] bytes = Utility.GetNextByteSet(data, 4 + head.Length, len);
+                    int check = BitConverter.ToInt32(data, len + 4 + head.Length);
+
+                    if (check == checksum(bytes))
+                    {
+                        packet = new Packet
+                        {
+                            Heading = header,
+                            Length = len,
+                            rawData = data,
+                            Checksum = check,
+                            data = bytes
+
+                        };
+                    }
+                }
+            }
+
+            return packet;
+        }
+
+        private static int checksum(byte[] data)
+        {
+            double value = 0;
+
+            for(int i = 0; i < data.Length; i++)
+            {
+                value += data[i];
+                value *= 53.97;
+                value %= int.MaxValue;
+            }
+
+            return (int)value;
+        }
+    }
+}
